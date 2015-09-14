4821294380e83697ab84cc3f6ca6735d4dafa281
Arthur MAURER <amaurer@e1r12p9.42.fr>
2015-09-14 21:55:37 +0200

    Commit

--- src/window.c
+++ src/window.c
@@ -1,5 +1,6 @@
 
 #include "scop.h"
+#include <stdio.h>
 
 extern t_scop	g_scop;
 
@@ -10,13 +11,9 @@ GLFWwindow	*create_window(void)
 	if (!glfwInit())
 		die("Could not init glfw.");
 
-	// glfwWindowHint(GLFW_SAMPLES, 4);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	// glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
